181e26ff7647ecd774be362f183b5a7bdcd51e33
xnie <xin@slimpay.com>
2016-05-12 16:38:26 +0200

    Replace PhantomJS by jsdom

--- build/karma.conf.js
+++ build/karma.conf.js
@@ -9,7 +9,6 @@ debug('Create configuration.')
 const karmaConfig = {
   basePath: '../', // project root in relation to bin/karma.js
   files: [
-    './node_modules/phantomjs-polyfill/bind-polyfill.js',
     {
       pattern: `./${config.dir_test}/test-bundler.js`,
       watched: false,
@@ -23,7 +22,7 @@ const karmaConfig = {
   preprocessors: {
     [`${config.dir_test}/test-bundler.js`]: ['webpack']
   },
-  browsers: ['PhantomJS'],
+  browsers: ['jsdom'],
   webpack: {
     devtool: 'cheap-module-source-map',
     resolve: {

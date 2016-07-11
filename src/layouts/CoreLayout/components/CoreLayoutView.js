9087a53d5d5406808f366276c1d38aec51b80343
xnie <xin@slimpay.com>
2016-07-11 15:46:31 +0200

    Update ant design

--- src/layouts/CoreLayout/components/CoreLayoutView.js
+++ src/layouts/CoreLayout/components/CoreLayoutView.js
@@ -4,7 +4,7 @@ import classes from './CoreLayout.scss'
 import 'styles/core.scss'
 // TODO: Move antd style to core.scss after following issue resolved
 // https://github.com/davezuko/react-redux-starter-kit/issues/474
-import 'antd/lib/index.css'
+import 'antd/dist/antd.css'
 
 export const CoreLayout = ({ children, localeChange }) => (
   <div className='container text-center'>
